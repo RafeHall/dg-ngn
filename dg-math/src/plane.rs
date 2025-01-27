fcae352c21a8e0b3f82ed7458843e3f3dd338e5b
RafeHall <rafeohall@gmail.com>
2025-01-26 21:30:06 -0800

    replaced TriPlane type with new_from_points_clockwise and new_from_points_counter_clockwise in Plane

--- dg-math/src/plane.rs
+++ dg-math/src/plane.rs
@@ -19,6 +19,26 @@ impl Plane {
         Self { normal, distance }
     }
 
+    pub fn from_points_clockwise(a: Vec3, b: Vec3, c: Vec3) -> Self {
+        let va = b.sub(a);
+        let vb = c.sub(a);
+
+        let normal = va.cross(vb).normalized().neg();
+        let distance = normal.dot(a);
+
+        Plane::new(normal, distance)
+    }
+
+    pub fn from_points_counter_clockwise(a: Vec3, b: Vec3, c: Vec3) -> Self {
+        let va = b.sub(a);
+        let vb = c.sub(a);
+
+        let normal = va.cross(vb).normalized();
+        let distance = normal.dot(a);
+
+        Plane::new(normal, distance)
+    }
+
     pub fn distance_to(&self, point: Vec3) -> Scalar {
         (self.normal.dot(point) - self.distance).abs()
     }
@@ -61,88 +81,88 @@ impl ApproxEq for Plane {
     }
 }
 
-impl Into<Triplane> for Plane {
-    fn into(self) -> Triplane {
-        todo!()
-    }
-}
+// impl Into<Triplane> for Plane {
+//     fn into(self) -> Triplane {
+//         todo!()
+//     }
+// }
 
-/// A plane defined by three clockwise points that lie on the plane
-#[derive(Debug, Clone, Copy)]
-pub struct Triplane {
-    pub a: Vec3,
-    pub b: Vec3,
-    pub c: Vec3,
-}
+// /// A plane defined by three clockwise points that lie on the plane
+// #[derive(Debug, Clone, Copy)]
+// pub struct Triplane {
+//     pub a: Vec3,
+//     pub b: Vec3,
+//     pub c: Vec3,
+// }
 
-impl Triplane {
-    // TODO: Ensure the order of vec3s is correct for sides of the plane on consts
-    /// Plane that splits the x-axis of the world
-    pub const X_INTERSECTION: Triplane = Triplane::new(Vec3::ZERO, Vec3::UP, Vec3::FORWARD);
-    /// Plane that splits the y-axis of the world
-    pub const Y_INTERSECTION: Triplane = Triplane::new(Vec3::ZERO, Vec3::FORWARD, Vec3::RIGHT);
-    /// Plane that splits the z-axis of the world
-    pub const Z_INTERSECTION: Triplane = Triplane::new(Vec3::ZERO, Vec3::RIGHT, Vec3::UP);
+// impl Triplane {
+//     // TODO: Ensure the order of vec3s is correct for sides of the plane on consts
+//     /// Plane that splits the x-axis of the world
+//     pub const X_INTERSECTION: Triplane = Triplane::new(Vec3::ZERO, Vec3::UP, Vec3::FORWARD);
+//     /// Plane that splits the y-axis of the world
+//     pub const Y_INTERSECTION: Triplane = Triplane::new(Vec3::ZERO, Vec3::FORWARD, Vec3::RIGHT);
+//     /// Plane that splits the z-axis of the world
+//     pub const Z_INTERSECTION: Triplane = Triplane::new(Vec3::ZERO, Vec3::RIGHT, Vec3::UP);
 
-    pub const fn new(a: Vec3, b: Vec3, c: Vec3) -> Self {
-        Self { a, b, c }
-    }
+//     pub const fn new(a: Vec3, b: Vec3, c: Vec3) -> Self {
+//         Self { a, b, c }
+//     }
 
-    pub fn distance_to(&self, point: Vec3) -> Scalar {
-        let n = self.normal();
-        let d = n.dot(self.a);
+//     pub fn distance_to(&self, point: Vec3) -> Scalar {
+//         let n = self.normal();
+//         let d = n.dot(self.a);
 
-        (n.dot(point) - d).abs()
-    }
+//         (n.dot(point) - d).abs()
+//     }
 
-    pub fn signed_distance_to(&self, point: Vec3) -> Scalar {
-        let n = self.normal();
-        let d = n.dot(self.a);
+//     pub fn signed_distance_to(&self, point: Vec3) -> Scalar {
+//         let n = self.normal();
+//         let d = n.dot(self.a);
 
-        n.dot(point) - d
-    }
+//         n.dot(point) - d
+//     }
 
-    pub fn side(&self, point: Vec3, eps: Scalar) -> Side {
-        match self.signed_distance_to(point) {
-            v if v > eps => Side::Outside,
-            v if v < -eps => Side::Inside,
-            _ => Side::On,
-        }
-    }
+//     pub fn side(&self, point: Vec3, eps: Scalar) -> Side {
+//         match self.signed_distance_to(point) {
+//             v if v > eps => Side::Outside,
+//             v if v < -eps => Side::Inside,
+//             _ => Side::On,
+//         }
+//     }
 
-    pub fn project(&self, point: Vec3) -> Vec3 {
-        point.sub(self.normal().mul_scalar(self.distance_to(point)))
-    }
+//     pub fn project(&self, point: Vec3) -> Vec3 {
+//         point.sub(self.normal().mul_scalar(self.distance_to(point)))
+//     }
 
-    pub fn normal(&self) -> Vec3 {
-        let a = self.b.sub(self.a);
-        let b = self.c.sub(self.a);
+//     pub fn normal(&self) -> Vec3 {
+//         let a = self.b.sub(self.a);
+//         let b = self.c.sub(self.a);
 
-        a.cross(b).normalized().neg()
-    }
-}
+//         a.cross(b).normalized().neg()
+//     }
+// }
 
-impl ApproxEq for Triplane {
-    fn approx_eq(&self, other: &Self) -> bool {
-        self.a.approx_eq(&other.a) && self.b.approx_eq(&other.b) && self.c.approx_eq(&other.c)
-    }
-}
+// impl ApproxEq for Triplane {
+//     fn approx_eq(&self, other: &Self) -> bool {
+//         self.a.approx_eq(&other.a) && self.b.approx_eq(&other.b) && self.c.approx_eq(&other.c)
+//     }
+// }
 
-impl Into<Plane> for Triplane {
-    fn into(self) -> Plane {
-        let normal = self.normal();
-        let distance = normal.dot(self.a);
+// impl Into<Plane> for Triplane {
+//     fn into(self) -> Plane {
+//         let normal = self.normal();
+//         let distance = normal.dot(self.a);
 
-        Plane::new(self.normal(), distance)
-    }
-}
+//         Plane::new(self.normal(), distance)
+//     }
+// }
 
 #[cfg(test)]
 mod tests {
     use crate::{assert_approx_eq, vector::Vec3, Side};
 
     use super::Plane;
-    use super::Triplane;
+    // use super::Triplane;
 
     #[test]
     fn distance_to() {
@@ -252,30 +272,30 @@ mod tests {
         assert_approx_eq!(r, Vec3::new(59182.0, 0.0, -25901.0));
     }
 
-    #[test]
-    fn into_plane() {
-        let tp = Triplane::X_INTERSECTION;
-        let p: Plane = tp.into();
+    // #[test]
+    // fn into_plane() {
+    //     let tp = Triplane::X_INTERSECTION;
+    //     let p: Plane = tp.into();
 
-        assert_approx_eq!(p, Plane::X_INTERSECTION);
+    //     assert_approx_eq!(p, Plane::X_INTERSECTION);
 
-        let tp = Triplane::Y_INTERSECTION;
-        let p: Plane = tp.into();
+    //     let tp = Triplane::Y_INTERSECTION;
+    //     let p: Plane = tp.into();
 
-        assert_approx_eq!(p, Plane::Y_INTERSECTION);
+    //     assert_approx_eq!(p, Plane::Y_INTERSECTION);
 
-        let tp = Triplane::Z_INTERSECTION;
-        let p: Plane = tp.into();
+    //     let tp = Triplane::Z_INTERSECTION;
+    //     let p: Plane = tp.into();
 
-        assert_approx_eq!(p, Plane::Z_INTERSECTION);
+    //     assert_approx_eq!(p, Plane::Z_INTERSECTION);
 
-        let tp = Triplane::new(
-            Vec3::new(1.0, 0.0, 0.0),
-            Vec3::new(1.0, 1.0, 0.0),
-            Vec3::new(1.0, 0.0, -1.0),
-        );
-        let p: Plane = tp.into();
+    //     let tp = Triplane::new(
+    //         Vec3::new(1.0, 0.0, 0.0),
+    //         Vec3::new(1.0, 1.0, 0.0),
+    //         Vec3::new(1.0, 0.0, -1.0),
+    //     );
+    //     let p: Plane = tp.into();
 
-        assert_approx_eq!(p, Plane::new(Vec3::RIGHT, 1.0));
-    }
+    //     assert_approx_eq!(p, Plane::new(Vec3::RIGHT, 1.0));
+    // }
 }
