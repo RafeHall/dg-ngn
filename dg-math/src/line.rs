dec725e7f9857beeae488a41c676cbbc1641d361
RafeHall <rafeohall@gmail.com>
2025-01-26 21:22:09 -0800

    reimplemented line-line intersection using homogenous coordinates

--- dg-math/src/line.rs
+++ dg-math/src/line.rs
@@ -1,9 +1,11 @@
-use std::ops::{IndexMut, Index};
+use std::ops::{Index, IndexMut};
 
-use super::{vector::Vec2, Scalar, interp::LinearInterp};
+use crate::{vector::Vec3, ApproxEq};
+
+use super::{interp::LinearInterp, vector::Vec2, Scalar};
 
 /// Line used for slicing
-#[derive(Debug, Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy)]
 pub struct Line {
     pub start: Vec2,
     pub end: Vec2,
@@ -13,12 +15,10 @@ impl Line {
     pub const CENTER_HORIZONTAL: Line = Line::new(Vec2::RIGHT, Vec2::LEFT);
     pub const CENTER_VERTICAL: Line = Line::new(Vec2::UP, Vec2::DOWN);
 
-    #[inline]
     pub const fn new(start: Vec2, end: Vec2) -> Line {
         Self { start, end }
     }
 
-    #[inline]
     pub fn new_from_normal(normal: Vec2, distance: Scalar) -> Line {
         let center = normal.mul_scalar(distance);
 
@@ -28,7 +28,6 @@ impl Line {
         }
     }
 
-    #[inline]
     pub fn new_from_slope_intercept(slope: Scalar, y_intercept: Scalar) -> Line {
         Self {
             start: Vec2::new(0.0, y_intercept),
@@ -36,12 +35,10 @@ impl Line {
         }
     }
 
-    #[inline]
     pub fn distance_from_origin(&self) -> Scalar {
         self.closest_point(Vec2::ZERO).length()
     }
 
-    #[inline]
     pub fn closest_point(&self, to: Vec2) -> Vec2 {
         let direction = self.direction();
         let offset = to.sub(self.end);
@@ -55,7 +52,6 @@ impl Line {
         }
     }
 
-    #[inline]
     pub fn segment_closest_point(&self, to: Vec2) -> Vec2 {
         let direction = self.direction();
         let offset = to.sub(self.end);
@@ -70,40 +66,41 @@ impl Line {
         }
     }
 
-    #[inline]
     pub fn segment(&self) -> Vec2 {
         self.end.sub(self.start)
     }
 
-    #[inline]
     pub fn direction(&self) -> Vec2 {
         self.start.direction_to(self.end)
     }
 
-    #[inline]
     pub fn normal(&self) -> Vec2 {
         self.direction().right()
     }
 
-    #[inline]
-    pub fn intersection(&self, other: Line) -> Vec2 {
-        let divisor = (self.start.x - self.end.x) * (other.start.y - other.end.y)
-            - (self.start.y - self.end.y) * (other.start.x - other.end.x);
-        
-        if divisor == 0.0 {
-            return Vec2::INFINITY;
+    pub fn intersection(&self, other: Line) -> Option<Vec2> {
+        let a = Vec3::new(
+            self.end.y - self.start.y,
+            self.start.x - self.end.x,
+            -self.start.x * (self.end.y - self.start.y)
+                + self.start.y * (self.end.x - self.start.x),
+        );
+
+        let b = Vec3::new(
+            other.end.y - other.start.y,
+            other.start.x - other.end.x,
+            -other.start.x * (other.end.y - other.start.y)
+                + other.start.y * (other.end.x - other.start.x),
+        );
+
+        let h = a.cross(b);
+        if h.z.approx_eq(&0.0) {
+            return None;
         }
 
-        let a = self.start.cross(self.end);
-        let b = other.start.cross(other.end);
-
-        let x_dividen = a * (other.start.x - other.end.x) - b * (self.start.x - self.end.x);
-        let y_dividen = a * (other.start.y - other.end.y) - b * (self.start.y - self.end.y);
-
-        Vec2::new(x_dividen, y_dividen).div_scalar(divisor)
+        Some(Vec2::new(h.x / h.z, h.y / h.z))
     }
 
-    #[inline]
     pub fn parallel(&self, other: Line, eps: Scalar) -> bool {
         let a = self.start.direction_to(self.end);
         let b = other.start.direction_to(other.end);
@@ -111,7 +108,6 @@ impl Line {
         (1.0 - a.dot(b).abs()).abs() < eps
     }
 
-    #[inline]
     pub fn perpendicular(&self, other: Line, eps: Scalar) -> bool {
         let a = self.start.direction_to(self.end);
         let b = other.start.direction_to(other.end);
@@ -119,22 +115,18 @@ impl Line {
         a.dot(b).abs() < eps
     }
 
-    #[inline]
     pub fn offset_x(&self, offset: Scalar) -> Line {
         self.offset(Vec2::new(offset, 0.0))
     }
 
-    #[inline]
     pub fn offset_y(&self, offset: Scalar) -> Line {
         self.offset(Vec2::new(0.0, offset))
     }
 
-    #[inline]
     pub fn offset(&self, offset: Vec2) -> Line {
         Line::new(self.start.add(offset), self.end.add(offset))
     }
 
-    #[inline]
     pub fn with_distance(&self, distance: Scalar) -> Line {
         let normal = if distance.is_sign_negative() {
             self.normal().neg()
@@ -146,11 +138,16 @@ impl Line {
         Line::new_from_normal(normal, distance)
     }
 
-    #[inline]
     pub fn with_normal(&self, normal: Vec2) -> Line {
         let distance = self.distance_from_origin();
 
         Line::new_from_normal(normal, distance)
+    }
+}
+
+impl ApproxEq for Line {
+    fn approx_eq(&self, other: &Self) -> bool {
+        self.start.approx_eq(&other.start) && self.end.approx_eq(&other.end)
     }
 }
 
@@ -182,52 +179,63 @@ impl IndexMut<usize> for Line {
 
 impl LinearInterp for Line {
     fn lerp(a: Self, b: Self, t: Scalar) -> Self {
-        Self::new(
-            a.start.lerp_to(b.start, t),
-            a.end.lerp_to(b.end, t)
-        )
+        Self::new(a.start.lerp_to(b.start, t), a.end.lerp_to(b.end, t))
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::vector::Vec2;
+    use crate::{assert_approx_eq, vector::Vec2};
 
     use super::Line;
 
     #[test]
     fn parallel_perpendicular() {
-        let a = Line::new(Vec2::new(5.0, 10.0), Vec2::new(10.0, 15.0));
-        let b = Line::new(Vec2::new(5.0, 20.0), Vec2::new(10.0, 25.0));
+        {
+            let a = Line::new(Vec2::new(5.0, 10.0), Vec2::new(10.0, 15.0));
+            let b = Line::new(Vec2::new(5.0, 20.0), Vec2::new(10.0, 25.0));
+    
+            assert!(a.parallel(b, 0.001), "{:?} should be parallel to {:?}", a, b);
+            assert!(!a.perpendicular(b, 0.001), "{:?} should not be perpendicular to {:?}", a, b);
+        }
 
-        assert!(a.parallel(b, 0.001));
-
-        let a = Line::new(Vec2::new(0.0, 5.0), Vec2::new(0.0, -10.0));
-        let b = Line::new(Vec2::new(5.0, 0.0), Vec2::new(-10.0, 0.0));
-
-        assert!(a.perpendicular(b, 0.001));
+        {
+            let a = Line::new(Vec2::new(0.0, 5.0), Vec2::new(0.0, -10.0));
+            let b = Line::new(Vec2::new(5.0, 0.0), Vec2::new(-10.0, 0.0));
+    
+            assert!(!a.parallel(b, 0.001), "{:?} should not be parallel to {:?}", a, b);
+            assert!(a.perpendicular(b, 0.001), "{:?} should be perpendicular to {:?}", a, b);
+        }
     }
 
     #[test]
     fn intersections() {
-        let a = Line::new(Vec2::new(0.0, 5.0), Vec2::new(0.0, -10.0));
-        let b = Line::new(Vec2::new(0.0, 10.0), Vec2::new(0.0, -15.0));
+        // Two lines that intersect at an integer position
+        {
+            let a = Line::new(Vec2::new(5.0, -5.0), Vec2::new(0.0, 5.0));
+            let b = Line::new(Vec2::new(1.0, -5.0), Vec2::new(1.0, 5.0));
+            let intersection = a.intersection(b).unwrap();
 
-        assert_eq!(a.intersection(b), Vec2::INFINITY);
+            assert_approx_eq!(intersection, Vec2::new(1.0, 3.0));
+        }
 
-        let a = Line::new(Vec2::new(0.0, 5.0), Vec2::new(0.0, -10.0));
-        let b = Line::new(Vec2::new(5.0, 0.0), Vec2::new(-10.0, 0.0));
+        // Two lines that intersect at a decimal position
+        {
+            let a = Line::new(Vec2::new(5.0, -15.0), Vec2::new(51.0, 5.0));
+            let b = Line::new(Vec2::new(25.0, 5.0), Vec2::new(1.0, 15.0));
+            let intersection = a.intersection(b).unwrap();
 
-        assert_eq!(a.intersection(b), Vec2::ZERO);
+            assert_approx_eq!(intersection, Vec2::new(38.2765957447, -0.531914893617));
+        }
 
-        let a = Line::new(Vec2::new(1.0, 1.0), Vec2::new(-1.0, -1.0));
-        let b = Line::new(Vec2::new(-1.0, 1.0), Vec2::new(1.0, -1.0));
+        // Parallel lines, do not intersect
+        {
+            let a = Line::new(Vec2::new(5.0, -15.0), Vec2::new(51.0, 5.0));
+            let b = a.clone().offset_x(5.0);
 
-        assert_eq!(a.intersection(b), Vec2::ZERO);
+            let intersection = a.intersection(b);
 
-        let a = Line::new(Vec2::new(2.0, 2.0), Vec2::new(-0.0, -0.0));
-        let b = Line::new(Vec2::new(-0.0, 2.0), Vec2::new(2.0, -0.0));
-
-        assert_eq!(a.intersection(b), Vec2::ONE);
+            assert!(intersection.is_none(), "{:?} should not intersect {:?}", a, b);
+        }
     }
 }
